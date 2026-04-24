8092243e1f9f5a0eda5a36ab1998efd62407ec82
Miles K <128091600+M-Ph484@users.noreply.github.com>
2026-04-24 13:34:17 -0700

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,2 +1,4 @@
-//intro - we learned how to do this already with js
+// Challenge 1: Hello World
+// Node.js can run JavaScript outside of a web browser.
+// The console.log() function sends text to the terminal so the user can see program output.
 console.log("Hello World!")
