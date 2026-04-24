c38bce11ed69f661d001edff87bd86a214f1729e
Miles K <128091600+M-Ph484@users.noreply.github.com>
2026-04-24 13:36:15 -0700

    Enhance comments for async file reading in my-first-async-io.js
    Added comments to explain the asynchronous file reading process and its functionality.

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,10 +1,31 @@
 //reference for help -https://nodejs.org/api/process.html#processargv 
 //and https://nodejs.org/api/fs.html#fsreadfilesyncpath-options
-var fs = require('fs')  
-var file = process.argv[2]  
-  
-fs.readFile(file, function (err, contents) {  
-  // fs.readFile(file, 'utf8', callback) can also be used  
-  var lines = contents.toString().split('\n').length - 1  
-  console.log(lines)  
+// Challenge 4: My First Async I/O
+// This program reads a file asynchronously and counts how many newline characters it contains.
+
+// require('fs') loads Node.js's built-in File System module.
+// The fs module lets this program read files from the computer.
+var fs = require('fs')
+
+// process.argv[2] stores the file path passed in from the terminal.
+// Example: node program.js file.txt
+// In that example, process.argv[2] would be "file.txt".
+var file = process.argv[2]
+
+// fs.readFile() reads the file asynchronously.
+// "Asynchronously" means Node.js starts reading the file, but does not freeze the entire program while waiting.
+// When the file is finished loading, Node.js runs the callback function below.
+fs.readFile(file, function (err, contents) {
+
+  // err would contain error information if something went wrong while reading the file.
+  // Learn You Node usually gives a valid file, so this solution does not need extra error handling.
+
+  // contents is returned as a Buffer, which is raw file data.
+  // toString() converts that Buffer into normal readable text.
+  // split('\n') breaks the text into an array wherever a newline character appears.
+  // The number of newline characters is one less than the number of array items, so we subtract 1.
+  var lines = contents.toString().split('\n').length - 1
+
+  // Print the final newline count to the terminal.
+  console.log(lines)
 })
