27f2cdad618b6e36ad976a332d351e55ebf4f2dd
Miles K <128091600+M-Ph484@users.noreply.github.com>
2026-04-24 13:38:59 -0700

    Enhance http-client.js with comments and structure
    Refactor HTTP client code with comments for clarity.

--- http-client.js
+++ http-client.js
@@ -1,17 +1,34 @@
 //used https://nodejs.org/api/http.html
 //and https://nodejs.org/api/http.html#httpgeturl-options-callback
-var http = require('http');
+// Challenge 7: HTTP Client
+// This program sends an HTTP GET request to a URL and prints the response data.
 
-var url = process.argv[2];
+// Load Node.js's built-in HTTP module.
+// The http module lets this program make web requests and receive web responses.
+var http = require('http')
 
-http.get(url, function(response) {
-    response.setEncoding('utf8');
-    
-    response.on('error', function(err) {
-       console.error(err);
-    });
-    
-    response.on('data', function(data) {
-        console.log(data);
-    });
-});
+// process.argv[2] stores the URL passed in from the terminal.
+// Example: node program.js http://example.com
+// In that example, process.argv[2] would be "http://example.com".
+var url = process.argv[2]
+
+// http.get() sends a GET request to the provided URL.
+// When the server responds, Node.js runs the callback function with the response object.
+http.get(url, function (response) {
+
+  // setEncoding('utf8') tells Node.js to treat incoming response data as readable text.
+  // Without this, the data may arrive as Buffer objects instead of normal strings.
+  response.setEncoding('utf8')
+
+  // The response object is a stream, meaning data can arrive in separate chunks over time.
+  // This event runs if there is an error while receiving the response.
+  response.on('error', function (err) {
+    console.error(err)
+  })
+
+  // The 'data' event runs whenever a chunk of response data is received.
+  // Learn You Node expects each chunk to be printed as it arrives.
+  response.on('data', function (data) {
+    console.log(data)
+  })
+})
